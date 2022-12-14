b14de620b2432ce6da85d7f2d44f5341fdff78e1
Diogo Alves <diogodidi2022@gmail.com>
2022-12-13 23:46:17 -0300

    components sidebar started

---
+++ src/components/Sidebar/SidebarElements.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const SidebarContainer = styled.div`
+
+`
+
+const Icon = styled.div`
+
+`
+
+const CloseIcon = styled.div`
+
+`
+
+export {
+    SidebarContainer,
+    Icon,
+    CloseIcon
+}
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,17 @@
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon
+} from "./SidebarElements";
+
+const Sidebar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar;
