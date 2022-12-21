dea88157304006b752d4446d82935ba1984007bf
Diogo Alves <diogodidi2022@gmail.com>
2022-12-21 02:22:00 -0300

    add more Footer component

--- src/components/Footer/FooterElements.tsx
+++ src/components/Footer/FooterElements.tsx
@@ -1,7 +1,76 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
+const FooterContainer = styled.footer`
+    background-color: #101522;
+`
+const FooterWrap = styled.div`
+    padding: 48px 24px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 1100px;
+    margin: 0 auto;
+`
+
+const FooterLinksContainer = styled.div`
+    display: flex;
+    justify-content: center;
+
+    @media(max-width: 820px) {
+        padding-top: 32px;
+    }
+`
+
+
+const FooterLinksWrapper = styled.div`
+    display: flex;
+
+    @media(max-width: 820px) {
+        flex-direction: column;
+    }
+`
+
+const FooterLinkItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 16px;
+    text-align: left;
+    width: 160px;
+    color: #FFF;
+
+    @media(max-width: 420px) {
+        margin: 0;
+        padding: 10px;
+        width: 100%;
+    }
+`
+
+const FooterLinkTitle = styled.h1`
+    font-size: 14px;
+    margin-bottom: 16px;
+`
+
+const FooterLink = styled(Link)`
+    color: #FFF;
+    text-decoration: none;
+    margin-bottom: .5rem;
+    font-size: 14px;
+
+    &:hover {
+        color: #01BF71;
+        transition: all .3s ease-out;
+    }
+`
 
 export {
-
-
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkTitle,
+    FooterLinkItems,
+    FooterLink
 };
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,45 +1,77 @@
+
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkTitle,
+    FooterLinkItems,
+    FooterLink
+} from "./FooterElements";
 const Footer = () => {
     return (
         <FooterContainer>
             <FooterWrap>
                 <FooterLinksContainer>
                     <FooterLinksWrapper>
-                        <FooterLinksItems>
+                        <FooterLinkItems>
                             <FooterLinkTitle>About Us</FooterLinkTitle>
                             <FooterLink to="/signin">How it Works</FooterLink>
                             <FooterLink to="/signin">Testimonials</FooterLink>
                             <FooterLink to="/signin">Careers</FooterLink>
                             <FooterLink to="/signin">Investors</FooterLink>
                             <FooterLink to="/signin">Terms od Service</FooterLink>
-                        </FooterLinksItems>
-                        <FooterLinksItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it Works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms od Service</FooterLink>
-                        </FooterLinksItems>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to="/">Contact</FooterLink>
+                            <FooterLink to="/">Support</FooterLink>
+                            <FooterLink to="/">Destinations</FooterLink>
+                            <FooterLink to="/">Sponsorships</FooterLink>                        
+                        </FooterLinkItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
-                        <FooterLinksItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it Works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms od Service</FooterLink>
-                        </FooterLinksItems>
-                        <FooterLinksItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it Works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms od Service</FooterLink>
-                        </FooterLinksItems>
-                    </FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to="/">Submit Video</FooterLink>
+                            <FooterLink to="/">Ambassadors</FooterLink>
+                            <FooterLink to="/">Agency</FooterLink>
+                            <FooterLink to="/">Influencer</FooterLink>                        
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to="/">Instagram</FooterLink>
+                            <FooterLink to="/">Facebook</FooterLink>
+                            <FooterLink to="/">YouTube</FooterLink>
+                            <FooterLink to="/">Twitter</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>                   
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="" >dolla</SocialLogo>
+                        <WebSiteRights>dolla &copy; {new Date().getFullYear()}
+                        All rights reserved.</WebSiteRights>
+                        <SocialIcons>
+                            <SocialLink href="/" target="_blank" arial-label="Facebook">
+                                <FaFacebook />
+                            </SocialLink>
+                            <SocialLink href="/" target="_blank" arial-label="Instagram">
+                                <FaInstagram/>
+                            </SocialLink>
+                            <SocialLink href="/" target="_blank" arial-label="Youtube">
+                                <FaYoutube />
+                            </SocialLink>
+                            <SocialLink href="/" target="_blank" arial-label="Twitter">
+                                <FaTwitter />
+                            </SocialLink>
+                            <SocialLink href="/" target="_blank" arial-label="LinkdIn">
+                                <FaLinkedin />
+                            </SocialLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
